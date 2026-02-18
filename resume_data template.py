5e33ce6a5ef1d989bb3e4c122bddbcd3de02c69a
Jules Programs <d8adp001@gmail.com>
2026-02-17 20:34:00 -0500

    Add initial resume structure and sample data files

---
+++ resume_data template.py
@@ -0,0 +1,19 @@
+# This file contains the resume data structure and sample data for testing.
+
+# Enter the resume data in the following format. This is just a sample structure and can be modified as needed.
+# This script is for my already formated resume data. I will be using this to test the resume generation and formatting functions.
+# However if you woish to use this as a template for your own resume data, feel free to modify the structure and content as needed.
+
+resume_data = {
+    "full_name": "",
+    "address": "",
+    "phone": "",
+    "email": "",
+    "github_url": "",
+    "linkedin_url": "",
+
+    "school_name": "",
+    "degree": "",
+    "graduation_date": "",
+    "gpa": "",
+}
